0df075bc6a09aad7f6bec507b44a73ddb3d85023
Everchavar <everchavarria07@gmail.com>
2026-03-25 17:42:18 -0600

    Estructura unificada

---
+++ academica/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+ Route::get('/saludo', function () {
+    return 'Bienvenidos a mi pagina, hola';
+});
