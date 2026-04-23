e04e63374a4a161f8c102c36f66fcbf549307f6e
Everchavar <everchavarria07@gmail.com>
2026-04-22 19:06:08 -0600

    inicio del proyecto mango

--- mango_music/routes/web.php
+++ mango_music/routes/web.php
@@ -5,6 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
- Route::get('/saludo', function () {
-    return 'Bienvenidos a mi pagina, hola';
-});
